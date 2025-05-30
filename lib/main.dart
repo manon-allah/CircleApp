64754918257401cf6671e06e9373121cffa544ef
menna allah <menatallah260@gmail.com>
2025-05-30 18:52:45 +0300

    add flutter_screenutil dependency and integrate ScreenUtilInit in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'features/first/presentation/pages/fisrt_page.dart';
 
 void main() {
@@ -10,15 +11,22 @@ class CircleApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Circle App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Tajawal',
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const FisrtPage(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return MaterialApp(
+          title: 'Circle App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            fontFamily: 'Tajawal',
+            scaffoldBackgroundColor: Colors.white,
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          home: const FisrtPage(),
+        );
+      },
     );
   }
 }
