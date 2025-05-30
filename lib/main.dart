2ef99643ec4ecee9df23c913e03429f44f52fac0
menna allah <menatallah260@gmail.com>
2025-05-30 18:53:54 +0300

    refactor: move CircleApp implementation to its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'features/first/presentation/pages/fisrt_page.dart';
+import 'circle_app.dart';
 
 void main() {
   runApp(const CircleApp());
-}
-
-class CircleApp extends StatelessWidget {
-  const CircleApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(360, 690),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (_, child) {
-        return MaterialApp(
-          title: 'Circle App',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            fontFamily: 'Tajawal',
-            scaffoldBackgroundColor: Colors.white,
-            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          ),
-          home: const FisrtPage(),
-        );
-      },
-    );
-  }
 }
