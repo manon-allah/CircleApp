2e8cd547cd4586337f176c37c3dcb820200fa755
menna allah <menatallah260@gmail.com>
2025-05-30 19:08:55 +0300

    integrate easy_localization for multi-language support and add language files

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:circle_app/core/routing/app_router.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'circle_app.dart';
 
-void main() {
-  runApp(CircleApp(
-    appRouter: AppRouter(),
-  ),);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('ar')],
+      path: 'assets/lang',
+      fallbackLocale: Locale('en'),
+      child: CircleApp(appRouter: AppRouter()),
+    ),
+  );
 }
