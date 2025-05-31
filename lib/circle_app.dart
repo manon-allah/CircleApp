f403ed7b758b3ba80bd4a663f9725bd9ee896357
menna allah <menatallah260@gmail.com>
2025-05-31 23:45:04 +0300

    add endlish font

--- lib/circle_app.dart
+++ lib/circle_app.dart
@@ -1,11 +1,12 @@
 import 'package:circle_app/core/routing/app_router.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'core/routing/routes.dart';
 
 class CircleApp extends StatelessWidget {
   final AppRouter appRouter;
-  const CircleApp({super.key, required this.appRouter,});
+  const CircleApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +21,16 @@ class CircleApp extends StatelessWidget {
           theme: ThemeData(
             fontFamily: 'Tajawal',
             scaffoldBackgroundColor: Colors.white,
+            appBarTheme: AppBarTheme(
+              backgroundColor: Colors.white,
+              foregroundColor: Colors.black,
+              elevation: 0,
+            ),
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           ),
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
           initialRoute: Routes.firstPage,
           onGenerateRoute: appRouter.generateRoute,
         );
